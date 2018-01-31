647031764d97b37411a44aefc74839ac632fc7d0
Jier <naturalwill999@gmail.com>
2018-01-31 18:14:47 +0800

    add plugins

---
+++ init_plugins.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/theme-next/theme-next-fancybox3 themes/next-reload/source/lib/fancybox
+
+git clone https://github.com/theme-next/theme-next-pace themes/next-reload/source/lib/pace
+
+git clone https://github.com/theme-next/theme-next-canvas-nest  themes/next-reload/source/lib/canvas-nest
