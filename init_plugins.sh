4ff0af6e717e19d47bc9a8e533d8a7f1fc93c536
Jier <naturalwill999@gmail.com>
2019-09-11 23:34:10 +0800

    add post and update themes/next-reload

--- init_plugins.sh
+++ init_plugins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/theme-next/theme-next-fancybox3 themes/next-reload/source/lib/fancybox
+#git clone https://github.com/theme-next/theme-next-fancybox3 themes/next-reload/source/lib/fancybox
 
 git clone https://github.com/theme-next/theme-next-pace themes/next-reload/source/lib/pace
 
